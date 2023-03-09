69ba05b06f9011c67cb31644f0dada1062714e8a
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-03-09 10:54:18 +0100

    feat: :sparkles: builded the home page

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -2,12 +2,49 @@ import Module from "./Module";
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-4xl text-red-600">Docker is awesome</h1>
-      <Module
-        title="Employees"
-        icon={<i className="fa-sharp fa-solid fa-user-tie"></i>}
-      />
+    <div className="h-screen">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center pt-32">
+        <Module
+          title="Employees"
+          icon={<i className="fa-sharp fa-solid fa-user-tie"></i>}
+        />
+
+        <Module
+          title="Contracts"
+          icon={<i className="fa-sharp fa-solid fa-file-contract"></i>}
+        />
+
+        <Module
+          title="Recrutment"
+          icon={<i className="fa-sharp fa-solid fa-user-plus"></i>}
+        />
+
+        <Module
+          title="Departments"
+          icon={<i className="fa-sharp fa-solid fa-building"></i>}
+        />
+
+        <Module
+          title="Holidays"
+          icon={<i className="fa-sharp fa-solid fa-gift"></i>}
+        />
+
+        <Module
+          title="Payslips"
+          icon={<i className="fa-sharp fa-solid fa-cash-register"></i>}
+        />
+      </div>
+      <div className="mt-24 flex justify-end px-12">
+        <button
+          type="button"
+          className="inline-flex items-center p-4 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          <i
+            className="fa-sharp fa-solid fa-right-from-bracket"
+            aria-hidden="true"
+          ></i>
+        </button>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Module.tsx
+++ frontend/src/components/Module.tsx
@@ -2,49 +2,18 @@ import { ModuleProps } from "../PropsTypes";
 
 const Module = ({ title, icon }: ModuleProps) => {
   return (
-    <div></div>
-    // <div className="max-w-sm">
-    //   <Card>
-    //     <div className="flex justify-end px-4 pt-4">
-    //       <Dropdown inline={true} label="">
-    //         <Dropdown.Item>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-    //           >
-    //             Edit
-    //           </a>
-    //         </Dropdown.Item>
-    //         <Dropdown.Item>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-    //           >
-    //             Export Data
-    //           </a>
-    //         </Dropdown.Item>
-    //         <Dropdown.Item>
-    //           <a
-    //             href="#"
-    //             className="block py-2 px-4 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-    //           >
-    //             Delete
-    //           </a>
-    //         </Dropdown.Item>
-    //       </Dropdown>
-    //     </div>
-    //     <div className="flex flex-col items-center pb-10">
-    //       <img
-    //         className="mb-3 h-24 w-24 rounded-full shadow-lg"
-    //         src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
-    //         alt="Bonnie image"
-    //       />
-    //       <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-    //         Bonnie Green
-    //       </h5>
-    //     </div>
-    //   </Card>
-    // </div>
+    <div className="w-full flex items-center justify-center pb-16">
+      <div className="sm:w-3/4 w-full 2xl:w-1/5 flex flex-col items-center py-16 md:py-12 border-2 hover:bg-blue-400 bg-blue-200 border-black rounded-lg cursor-pointer">
+        <div className="w-full flex items-center justify-center">
+          <div className="flex flex-col items-center text-6xl">
+            {icon}
+            <p className="mt-2 text-xs sm:text-sm md:text-base font-semibold text-center">
+              {title}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
