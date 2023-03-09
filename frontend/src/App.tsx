5c78701e438d3406b8f1ebfd4ad0f6aa55440fd0
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-03-09 11:51:30 +0100

    feat: :sparkles: added login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
